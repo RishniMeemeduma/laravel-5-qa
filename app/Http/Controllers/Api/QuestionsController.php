4f964839450efb5dd5f1474c6084f4453c3ac483
Rishni <ashameemeduma@gmail.com>
2021-08-24 20:20:59 +0530

    Create update & delete question endpoints

--- app/Http/Controllers/Api/QuestionsController.php
+++ app/Http/Controllers/Api/QuestionsController.php
@@ -45,7 +45,12 @@ class QuestionsController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        
+        return response()->json([
+            'title'     => $question->title,
+            'body'      => $question->body,
+            'body_html' => $question->body_html
+        ]);
     }
 
     /**
@@ -55,9 +60,15 @@ class QuestionsController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $this->authorize('update',$question);
+        $question->update($request->only(['title','body']));
+
+        return response()->json([
+            'message'=>"Your question has been updated.",
+            'body_html' => $question->body_html
+        ]);
     }
 
     /**
@@ -68,6 +79,11 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $this->authorize('delete',$question);
+        $question->delete();
+
+        return response()->json([
+            'message' =>"Queston deleted successfully"
+        ]);
     }
 }
